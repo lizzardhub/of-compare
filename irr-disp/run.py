75316a91bc21466e3a56c4aada3853bad20d1d14
iternal <iternal@example.com>
2020-06-30 20:36:18 +0300

    message

---
+++ irr-disp/run.py
@@ -0,0 +1,13 @@
+import subprocess
+from glob import glob
+import shutil
+
+process = subprocess.Popen('cd scripts/validation; bash IRR-PWC_things3d.sh', shell=True, stdout=subprocess.PIPE)
+process.wait()
+
+print(process.returncode)
+
+for filepath in sorted(glob('/content/irr/saved_check_point/pwcnet/eval_temp/IRR_PWC/flo/frames/in/*')):
+    filename = filepath.split('/')[-1]
+    #shutil.copyfile(filepath, '/content/sintelall/MPI-Sintel-complete/training/frames/out/frame_' + filename.split('_')[1] + '.png')
+    shutil.copyfile(filepath, '/content/sintelall/MPI-Sintel-complete/training/frames/out/' + filename)
