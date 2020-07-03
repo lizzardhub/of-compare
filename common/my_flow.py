4b899e8f2270b828656b9717b271d9e596eb9276
user.name <you@example.com>
2020-07-03 16:26:42 +0300

    3

--- common/my_flow.py
+++ common/my_flow.py
@@ -388,7 +388,9 @@ def split_frames(stereo=False):
     for i, filepath in enumerate(sorted(glob('frames/*'))):
         print(filepath)
         img = skimage.img_as_float(imread(filepath))[bounds[0]:bounds[1], bounds[2]:bounds[3]]
+        
         #img = img[::2, ::2]
+        img = cv2.resize(img, (w // 2, h // 2))
 
         if not(stereo and i % 2 == 1):
             img, params = distortion(img, params)
