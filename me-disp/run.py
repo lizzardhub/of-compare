93aa66adb4d0b1398856da826e157b127cbd2ab8
user.name <you@example.com>
2020-07-03 21:39:37 +0300

    1

--- me-disp/run.py
+++ me-disp/run.py
@@ -39,7 +39,7 @@ print(np.sum(np.abs(v)))'''
 def estimate_disp(processor, img_l, img_r):
     processor.EstimateDisp(img_l, img_r)
     f = processor.GetDisparityMap()
-    u, v = f[1][0], f[1][1]
+    u, v = f[0][0], f[0][1]
     flow = np.stack([u, v], axis=2).astype(np.float32)
     return flow
 
@@ -49,12 +49,10 @@ img_r = io.imread(image_list[1])
 
 h, w = img_l.shape[:2]
 
-max_h = h
-max_w = w
-#max_h = int(h // 16 * 16)
-#max_w = int(w // 16 * 16)
-#if max_h < h: max_h += 16
-#if max_w < w: max_w += 16
+max_h = int(h // 16 * 16)
+max_w = int(w // 16 * 16)
+if max_h < h: max_h += 16
+if max_w < w: max_w += 16
 
 processor = DE(max_w, max_h)
 
@@ -64,18 +62,18 @@ for i in range(0, len(image_list) - 1, 2):
     img_r = io.imread(image_list[i + 1])
     print('Image paths:', image_list[i], image_list[i + 1])
 
-    #img_l = cv2.resize(img_l,(max_w, max_h))
-    #img_r = cv2.resize(img_r,(max_w, max_h))
+    img_l = cv2.resize(img_l,(max_w, max_h))
+    img_r = cv2.resize(img_r,(max_w, max_h))
 
     tb = time()
     flow = estimate_disp(processor, img_l, img_r)
-    #flow = cv2.resize(flow, (w, h))
+    flow = cv2.resize(flow, (w, h))
     write_flow(flow, './images/out/' + fname.split('.')[0] + '.flo')
     print('{:.3f} seconds elapsed'.format(time() - tb))
 
     tb = time()
     flow = estimate_disp(processor, img_r, img_l)
-    #flow = cv2.resize(flow, (w, h))
+    flow = cv2.resize(flow, (w, h))
     write_flow(flow, './images/out/' + fname.split('.')[0] + '_b.flo')
     print('{:.3f} seconds elapsed'.format(time() - tb))
     #io.imsave('./images/out/' + fname, flow_to_image(flow))
