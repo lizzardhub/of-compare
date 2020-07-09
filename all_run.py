bd052511509b763eb260f1dcc01f2feae908dda1
user.name <you@example.com>
2020-07-09 10:42:53 +0300

    3

--- all_run.py
+++ all_run.py
@@ -10,7 +10,7 @@ disp = True
 
 #lst = sorted(glob('vids/*_l*'))
 #for i in [1, 11, 15, 20, 24, 25, 38, 82]:
-for id in ['01_l4', '05_l4', '06_l1', '06_l2', '06_l3', '06_l4', '07_l4', '08_l0']:
+for id in ['11_l1', '11_l2', '11_l3', '11_l4', '12_l1', '12_l2', '12_l3', '12_l4', , '13_l2', '16_l4']:
 #for video_path in sorted(glob('vids/*_l*'))[START_IDX:STOP_IDX]:
     #video_path = lst[i]
     video_path = 'vids/' + id + '.mkv'
